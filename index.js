e940d27e9e7e33a352312cea3c7d96546344630b
Daniel Crego <dcrego90@gmail.com>
2018-05-28 22:15:14 +0200

    index.html

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-function index(req, res) {
-    res.send('Hello World!');
-}
-app.get('/', index);
+app.use(express.static('public'));
+app.get(/\/$/, (req, res) => {
+    res.redirect('index.html');
+});
 
 app.listen(80, ()=>{
     console.log(`cli-mate app running on http://localhost:${port}/`);
