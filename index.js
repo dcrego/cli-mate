4378c53daf693d084056c3fd7dc5a61490fd6473
Daniel Crego <dcrego90@gmail.com>
2018-05-29 20:11:51 +0200

    Styles

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 80;
 
 app.use(express.static('public'));
-app.get(/\/$/, (req, res) => {
-    res.redirect('index.html');
+app.get('/', (req, res) => {
+    res.redirect('/index.html');
 });
 
 app.listen(80, ()=>{
