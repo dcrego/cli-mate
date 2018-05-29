904a9dc22fd103361a14e9050daeb834030c0b81
Daniel Crego <dcrego90@gmail.com>
2018-05-30 00:28:27 +0200

    Current Weather Data

--- public/current-weather.js
+++ public/current-weather.js
@@ -40,4 +40,31 @@ const sample = {
     "name": "Cairns",
     "cod": 200
 };
-// $('#current-weather').text(JSON.stringify(sample));
+const currentWeather = $('#currentWeather');
+if (sample.cod==200) {
+    currentWeather.find('.w-icon').attr('src', `https://openweathermap.org/img/w/${sample.weather[0].icon}.png`);
+    currentWeather.find('.w-description').text(sample.weather[0].description);
+    var temp = sample.main.temp;
+    currentWeather.find('.m-temp-k').text(temp);
+    temp = temp - 273.15;
+    currentWeather.find('.m-temp-c').text(temp);
+    currentWeather.find('.m-humid').text(sample.main.humidity);
+    currentWeather.find('.m-press').text(sample.main.pressure);
+    currentWeather.find('.wind-s').text(sample.wind.speed);
+    currentWeather.find('.wind-r').text(sample.wind.deg);
+    currentWeather.find('.clouds').text(sample.clouds.all);
+    var time = {h:0, m:0, s:sample.dt};
+    time.m = Math.floor(time.s/60);
+    time.s %= 60;
+    time.h = Math.floor(time.m / 60);
+    time.m %= 60;
+    time.h %= 24;
+    for (const key in time) {
+        var s = time[key].toString();
+        if (s.length<2) {
+            s = '0'+s;
+        }
+        time[key] = s;
+    }
+    currentWeather.find('.time').text(`${time.h}:${time.m}:${time.s}`);
+}
